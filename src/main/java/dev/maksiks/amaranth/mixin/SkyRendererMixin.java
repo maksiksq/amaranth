aaa1eeab049ee6a844b3af96cb260ea4d9feeca4
Maksiks <maksiksking@gmail.com>
2025-09-20 15:06:34 +0300

    fix: killed the fucking sun

---
+++ src/main/java/dev/maksiks/amaranth/mixin/SkyRendererMixin.java
@@ -0,0 +1,29 @@
+package dev.maksiks.amaranth.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.maksiks.amaranth.worldgen.biome.ModBiomes;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class SkyRendererMixin {
+    @Inject(method = "renderSky", at = @At("HEAD"), cancellable = true)
+    private void onRenderSky(Matrix4f matrix4f, Matrix4f projectionMatrix, float partialTick, Camera camera, boolean isFoggy, Runnable skyFogSetup, CallbackInfo ci) {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player == null || mc.level == null) return;
+
+        ResourceKey<Biome> biome = mc.level.getBiome(mc.player.blockPosition()).unwrapKey().orElse(null);
+
+        if (biome != null && biome.equals(ModBiomes.DESOLATE_ICE_FIELDS)) {
+            ci.cancel();
+        }
+    }
+}
